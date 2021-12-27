8d297fdff9a25419eecf187c8b2f228da3af698f
kadirovgm <kadirovgm@gmail.com>
2021-12-27 09:57:38 +0300

    upd

---
+++ alg_adq/array_squares.java
@@ -0,0 +1,50 @@
+// Complexity O(n*logn)
+public class array_squares {
+    public int[] sortedSquares(int[] A) {
+    
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
+
+// Complexity O(n)
+// Using two pointers
+class Solution {
+    public int[] sortedSquares(int[] A) 
+        int[] result = new int[A.length];
+        int s = 0; // new array
+        int i = 0; // left
+        int j = 0; // right
+for (int k = 0; k < A.length; k++) {
+            if (A[k] < 0) {
+                j++;
+            }
+            A[k] = A[k] * A[k];
+        }
+        i = j -1;
+        while (i >= 0 && j <= A.length - 1) {
+            if (A[i] < A[j]) {
+                result[s] = A[i];
+                i--;
+            } else {
+                result[s] = A[j];
+                j++;
+            }
+            s++;
+        }
+        while (i >= 0) {
+            result[s] = A[i];
+            i--;
+            s++;
+        }
+        while (j <= A.length - 1) {
+            result[s] = A[j];
+            j++;
+            s++;
+        }
+        return result;
+    }
+}
