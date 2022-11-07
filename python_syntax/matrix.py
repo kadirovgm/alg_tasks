b6576875d66f2d35aaa3e227bc4c8825feee962b
kadirovgm <kadirovgm@gmail.com>
2022-11-08 00:02:10 +0300

    matrix squared

--- python_syntax/matrix.py
+++ python_syntax/matrix.py
@@ -15,3 +15,13 @@ for i in range(n):
 
 # Square Matrix input
 matrix = [[input()]*n for _ in range(n)]
+
+
+# read matrix
+n = int(input())
+matrix = []
+for i in range(n):
+    temp = [int(num) for num in input().split()]
+    matrix.append(temp)
+
+print(matrix)
